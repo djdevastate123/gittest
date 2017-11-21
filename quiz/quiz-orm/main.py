0ea59b567ec67b6397cb1764825b6c9ef0e3fa6e
kl3ag1 <kl3ag1@komp16.lo1cg.org>
2017-11-21 12:35:57 +0100

    Strona ło la boga

--- quiz/quiz-orm/main.py
+++ quiz/quiz-orm/main.py
@@ -4,5 +4,17 @@
 from app import app
 from views import *
 
+import os
+from app import app, baza
+from models import *
+from views import *
+from dane import *
+
+if __name__ == '__main__':
+    if not os.path.exists(app.config['DATABASE']):
+        baza.create_tables([Pytanie, Odpowiedz], True)  # tworzymy tabele
+        dodaj_pytania(pobierz_dane('pytania.csv'))
+    app.run(debug=True)
+
 if __name__ == '__main__':
     app.run(debug=True)
