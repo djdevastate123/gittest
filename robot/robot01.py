7ac7ef8b60fe53c7e64e3de0167cac90b8afcefa
djdevastate123 <krzysiek.st12@vp.pl>
2018-03-06 14:21:50 +0100

    asasf

--- robot/robot01.py
+++ robot/robot01.py
@@ -7,23 +7,42 @@ import rg
 class Robot:
 
     def act(self, game):
+        # rodzaje pól: 'spawn', 'normal', 'obstacle', 'invalid'
+        # rg.loc_types() - zwraca typ pola
+        def czy_wejscie(poz):
+            if 'spawn' in rg.loc_types(poz):
+                return True
+            return False
+
+        # return ['guard']
+        # return ['suicide']
+        # return ['move', ()]
         # ilu_wrogow = 0
-        lista_wrogow = []
+
+        print(game.robots)
+
+        lista_wrogow_obok = []
 
         for poz, robot in game.robots.iteritems():
-            if robot.player_id != self.player_id:
+            if robot.player_id != self.player_id:  # rozpoznanie wroga
                 if rg.dist(poz, self.location) <= 1:
-                    lista_wrogow.append(poz)
+                    lista_wrogow_obok.append(poz)
                     # ilu_wrogow +=1
                     # return ['attack', poz]
 
-        if len(lista_wrogow) > 2:
+        print(lista_wrogow_obok)
+
+        # rg.dist() - odległość między dwoma lokalizacjami
+        # rg.toward() - najkrótsza droga między dwoma lokalizacjami
+
+        if len(lista_wrogow_obok) > 2 and self.hp < 27 :
             return['suicide']
-        elif len(lista_wrogow):
-            return['attack', lista_wrogow[0]]
-        print(game.robots)
+        elif len(lista_wrogow_obok):
+            return['attack', lista_wrogow_obok[0]]
 
         if self.location == rg.CENTER_POINT:
             return ['suicide']
+
         # idź do środka planszy, ruch domyślny
-        return ['move', rg.toward(self.location, rg.CENTER_POINT)]
+        if czy_wejscie(self.poz):
+            return ['move', rg.toward(self.location, rg.CENTER_POINT)]
